fdf8928ec6f5f05bc8145ea83fd30212fa95f28c
Aasha Medhi <aasha.medhi2004@gmail.com>
2020-02-18 08:45:09 +0530

    HOTFIX-3631 BUG-124588 HIVE-22844 : Validate cm configs, add retries in fs apis for cm.  (Aasha Medhi, reviewed by Mahesh Kumar Behera)
    Signed-off-by: Mahesh Kumar Behera <mahesh@apache.org>
    (cherry picked from commit 21f801dc82b93eb9c6ee449be15a003cd228e02d)
    
    Change-Id: I02cc0805f86bf193498e04a0f7270170d6741039

--- standalone-metastore/src/main/java/org/apache/hadoop/hive/metastore/HiveMetaStore.java
+++ standalone-metastore/src/main/java/org/apache/hadoop/hive/metastore/HiveMetaStore.java
@@ -2900,27 +2900,7 @@ public class HiveMetaStore extends ThriftHiveMetastore {
      */
     private void deleteTableData(Path tablePath, boolean ifPurge, boolean shouldEnableCm) {
       if (tablePath != null) {
-        try {
-          if (shouldEnableCm) {
-            //Don't delete cmdir if its inside the table path
-            FileStatus[] statuses = tablePath.getFileSystem(conf).listStatus(tablePath,
-                    ReplChangeManager.CMROOT_PATH_FILTER);
-            for (final FileStatus status : statuses) {
-              wh.deleteDir(status.getPath(), true, ifPurge, shouldEnableCm);
-            }
-            //Check if table directory is empty, delete it
-            FileStatus[] statusWithoutFilter = tablePath.getFileSystem(conf).listStatus(tablePath);
-            if (statusWithoutFilter.length == 0) {
-              wh.deleteDir(tablePath, true, ifPurge, shouldEnableCm);
-            }
-          } else {
-            //If no cm delete the complete table directory
-            wh.deleteDir(tablePath, true, ifPurge, shouldEnableCm);
-          }
-        } catch (Exception e) {
-          LOG.error("Failed to delete table directory: " + tablePath +
-                  " " + e.getMessage());
-        }
+        deleteDataExcludeCmroot(tablePath, ifPurge, shouldEnableCm);
       }
     }
 
@@ -2958,28 +2938,41 @@ public class HiveMetaStore extends ThriftHiveMetastore {
     private void deletePartitionData(List<Path> partPaths, boolean ifPurge, boolean shouldEnableCm) {
       if (partPaths != null && !partPaths.isEmpty()) {
         for (Path partPath : partPaths) {
-          try {
-            if (shouldEnableCm) {
-              //Don't delete cmdir if its inside the partition path
-              FileStatus[] statuses = partPath.getFileSystem(conf).listStatus(partPath,
-                      ReplChangeManager.CMROOT_PATH_FILTER);
-              for (final FileStatus status : statuses) {
-                wh.deleteDir(status.getPath(), true, ifPurge, shouldEnableCm);
-              }
-              //Check if partition directory is empty, delete it
-              FileStatus[] statusWithoutFilter = partPath.getFileSystem(conf).listStatus(partPath);
-              if (statusWithoutFilter.length == 0) {
-                wh.deleteDir(partPath, true, ifPurge, shouldEnableCm);
-              }
-            } else {
-              //If no cm delete the complete partition directory
-              wh.deleteDir(partPath, true, ifPurge, shouldEnableCm);
-            }
-          } catch (Exception e) {
-            LOG.error("Failed to delete partition directory: " + partPath +
-                    " " + e.getMessage());
-          }
+          deleteDataExcludeCmroot(partPath, ifPurge, shouldEnableCm);
         }
+      }
+    }
+
+    /**
+     * Delete data from path excluding cmdir
+     * and for each that fails logs an error.
+     *
+     * @param path
+     * @param ifPurge completely purge the partition (skipping trash) while
+     *                removing data from warehouse
+     * @param shouldEnableCm If cm should be enabled
+     */
+    private void deleteDataExcludeCmroot(Path path, boolean ifPurge, boolean shouldEnableCm) {
+      try {
+        if (shouldEnableCm) {
+          //Don't delete cmdir if its inside the partition path
+          FileStatus[] statuses = path.getFileSystem(conf).listStatus(path,
+                  ReplChangeManager.CMROOT_PATH_FILTER);
+          for (final FileStatus status : statuses) {
+            wh.deleteDir(status.getPath(), true, ifPurge, shouldEnableCm);
+          }
+          //Check if table directory is empty, delete it
+          FileStatus[] statusWithoutFilter = path.getFileSystem(conf).listStatus(path);
+          if (statusWithoutFilter.length == 0) {
+            wh.deleteDir(path, true, ifPurge, shouldEnableCm);
+          }
+        } else {
+          //If no cm delete the complete table directory
+          wh.deleteDir(path, true, ifPurge, shouldEnableCm);
+        }
+      } catch (Exception e) {
+        LOG.error("Failed to delete directory: " + path +
+                " " + e.getMessage());
       }
     }
 
