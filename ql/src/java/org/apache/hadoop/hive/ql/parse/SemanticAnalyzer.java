8a716e2b48d209d36e8ad8b00d5e1c28d948e8ef
Zoltan Haindrich <kirk@rxd.hu>
2020-07-08 16:51:30 +0200

    HOTFIX-3554 BUG-124565 HIVE-22301: Hive lineage is not generated for insert overwrite queries on partitioned tables (Zoltan Haindrich reviewed by Denys Kuzmenko and Jesus Camacho Rodriguez)
    (cherry picked from commit ac254b86236d81c336eb1b4063e85156908f9223)
    
    Change-Id: Ibc65b9d5210e4f934dc2c406e43769fd41cfd4f5

--- ql/src/java/org/apache/hadoop/hive/ql/parse/SemanticAnalyzer.java
+++ ql/src/java/org/apache/hadoop/hive/ql/parse/SemanticAnalyzer.java
@@ -7979,7 +7979,7 @@ public class SemanticAnalyzer extends BaseSemanticAnalyzer {
     if ((dpCtx != null) && (dpCtx.getNumDPCols() >= 0)) {
       // No static partition specified
       if (dpCtx.getNumSPCols() == 0) {
-        output = new WriteEntity(dest_tab, determineWriteType(ltd, isNonNativeTable, dest), false);
+        output = new WriteEntity(dest_tab, determineWriteType(ltd, isNonNativeTable, dest), true);
         outputs.add(output);
         output.setDynamicPartitionWrite(true);
       }
