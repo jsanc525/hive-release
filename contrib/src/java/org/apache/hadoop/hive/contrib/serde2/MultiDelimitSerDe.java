89c3d46c254051cb0508874f5ea7b2300d9efce0
Adam Szita <40628386+szlta@users.noreply.github.com>
2020-09-17 10:06:46 +0200

    BUG-124493 HIVE-24151: MultiDelimitSerDe shifts data if strings contain non-ASCII characters (Adam Szita, reviewed by Peter Vary)
    Change-Id: Ib774a52544666c0506af5b0e9840fcb054841f09
    (cherry picked from commit 34a35b256acfff72c74eaea5defb268b47ffc88b)

--- contrib/src/java/org/apache/hadoop/hive/contrib/serde2/MultiDelimitSerDe.java
+++ contrib/src/java/org/apache/hadoop/hive/contrib/serde2/MultiDelimitSerDe.java
@@ -70,7 +70,8 @@ public class MultiDelimitSerDe extends AbstractEncodingAwareSerDe {
   private static final String COLLECTION_DELIM = "collection.delim";
 
   // actual delimiter(fieldDelimited) is replaced by REPLACEMENT_DELIM in row.
-  private static final String REPLACEMENT_DELIM = "\1";
+  public static final String REPLACEMENT_DELIM_SEQUENCE = "\1";
+  public static final int REPLACEMENT_DELIM_LENGTH = REPLACEMENT_DELIM_SEQUENCE.getBytes().length;
 
   private int numColumns;
   private String fieldDelimited;
@@ -93,8 +94,6 @@ public class MultiDelimitSerDe extends AbstractEncodingAwareSerDe {
   private final ByteStream.Output serializeStream = new ByteStream.Output();
   // The Writable to return in serialize
   private final Text serializeCache = new Text();
-  // pattern for delimiter
-  private Pattern delimiterPattern;
 
   @Override
   public void initialize(Configuration conf, Properties tbl) throws SerDeException {
@@ -106,7 +105,6 @@ public class MultiDelimitSerDe extends AbstractEncodingAwareSerDe {
     if (fieldDelimited == null || fieldDelimited.isEmpty()) {
       throw new SerDeException("This table does not have serde property \"field.delim\"!");
     }
-    delimiterPattern = Pattern.compile(fieldDelimited, Pattern.LITERAL);
 
     // get the collection separator and map key separator
     // TODO: use serdeConstants.COLLECTION_DELIM when the typo is fixed
@@ -142,7 +140,7 @@ public class MultiDelimitSerDe extends AbstractEncodingAwareSerDe {
   }
 
 
-  @Override 
+  @Override
   public Object doDeserialize(Writable blob) throws SerDeException {
     if (byteArrayRef == null) {
       byteArrayRef = new ByteArrayRef();
@@ -160,14 +158,14 @@ public class MultiDelimitSerDe extends AbstractEncodingAwareSerDe {
     } else {
       throw new SerDeException(getClass() + ": expects either BytesWritable or Text object!");
     }
-    byteArrayRef.setData(rowStr.replaceAll(Pattern.quote(fieldDelimited), REPLACEMENT_DELIM).getBytes());
+    byteArrayRef.setData(rowStr.replaceAll(Pattern.quote(fieldDelimited), REPLACEMENT_DELIM_SEQUENCE).getBytes());
     cachedLazyStruct.init(byteArrayRef, 0, byteArrayRef.getData().length);
     // use the multi-char delimiter to parse the lazy struct
-    cachedLazyStruct.parseMultiDelimit(rowStr, delimiterPattern, REPLACEMENT_DELIM);
+    cachedLazyStruct.parseMultiDelimit(rowStr.getBytes(), fieldDelimited.getBytes(), REPLACEMENT_DELIM_LENGTH);
     return cachedLazyStruct;
   }
 
-  @Override 
+  @Override
   public Writable doSerialize(Object obj, ObjectInspector objInspector)
       throws SerDeException {
     StructObjectInspector soi = (StructObjectInspector) objInspector;
